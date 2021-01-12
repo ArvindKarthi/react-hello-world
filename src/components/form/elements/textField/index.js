46ef2295cafac3e3d4fb4e24b43f17964af0774a
iamarvind <varoonarvind287054@gmail.com>
2021-01-12 12:19:50 +0530

    to add password type to textfield component

--- src/components/form/elements/textField/index.js
+++ src/components/form/elements/textField/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const TextField = ({ attributes }) => {
-  const { label, size, placeholder } = attributes;
+  const { type, label, size, placeholder } = attributes;
   return (
     <div className="form-group">
       <label htmlFor={label} className={`d-block fn-${size} form-label`}>
         {label}
       </label>
       <input
-        type="text"
+        type={type}
         name={label}
         className={`form-control form-control-${size} `}
         id={label}
--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,24 @@
 // attributes for each text fields that would make each textField component different.
 const textFieldAttributes = [
-  { label: "Username", size: "small", placeholder: "fullname" },
-  { label: "Interest", size: "small", placeholder: "interest" },
-  { label: "Email", size: "small", placeholder: "email" },
-  { label: "Phone", size: "small", placeholder: "phone no" },
-  { label: "College", size: "small", placeholder: "college name" },
-  { label: "Department", size: "small", placeholder: " department" },
+  { label: "Username", size: "small", placeholder: "fullname", type: "text" },
+  {
+    label: "Password",
+    size: "small",
+    placeholder: "password",
+    type: "password",
+  },
+  {
+    label: "College",
+    size: "small",
+    placeholder: "college name",
+    type: "text",
+  },
+  {
+    label: "Department",
+    size: "small",
+    placeholder: " department",
+    type: "text",
+  },
 ];
 
 const Config = { textFieldAttributes };
